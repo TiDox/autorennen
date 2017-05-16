930fc6109701b131c5af8dc7e85d1ec333147978
TiDox <info@tidox.de>
2017-05-16 11:09:10 +0200

    Add files via upload

--- src/autorennen/MainMenu.java
+++ src/autorennen/MainMenu.java
@@ -5,17 +5,38 @@
  */
 package autorennen;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 /**
  *
  * @author bbsuser
  */
-public class MainMenu extends javax.swing.JFrame {
+public class MainMenu extends javax.swing.JFrame implements KeyListener {
 
     /**
      * Creates new form MainMenu
      */
     public MainMenu() {
         initComponents();
+        
+        Auto autoSpieler1 = new Auto(this.spielerName1.getText());
+        Auto autoSpieler2 = new Auto(this.spielerName2.getText());
+        
+        ImageIcon imgAuto = new ImageIcon("Pizza_car..png");
+        auto2.setIcon(imgAuto);
+        
+        this.tankFuellung1.setValue(100);
+        this.tankFuellung2.setValue(100);
     }
 
     /**
@@ -27,20 +48,33 @@ public class MainMenu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jFrame1 = new javax.swing.JFrame();
         spielerName1 = new javax.swing.JLabel();
         spielerName2 = new javax.swing.JLabel();
         tankFuellung2 = new javax.swing.JProgressBar();
         tankFuellung1 = new javax.swing.JProgressBar();
         streckeSpieler1 = new javax.swing.JPanel();
         streckeSpieler2 = new javax.swing.JPanel();
-        drehzahlMesser2 = new javax.swing.JProgressBar();
-        drehzahlMesser1 = new javax.swing.JProgressBar();
-        schalten2 = new javax.swing.JButton();
-        schalten1 = new javax.swing.JButton();
+        auto2 = new javax.swing.JLabel();
+        bremsen2 = new javax.swing.JButton();
+        bremsen1 = new javax.swing.JButton();
         gasGeben2 = new javax.swing.JButton();
         gasGeben1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         countdown = new javax.swing.JLabel();
+        weiter1 = new javax.swing.JButton();
+        fertig = new javax.swing.JButton();
+
+        javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
+        jFrame1.getContentPane().setLayout(jFrame1Layout);
+        jFrame1Layout.setHorizontalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jFrame1Layout.setVerticalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -67,24 +101,33 @@ public class MainMenu extends javax.swing.JFrame {
 
         streckeSpieler2.setBackground(new java.awt.Color(255, 0, 240));
 
+        auto2.setText("auto2");
+
         javax.swing.GroupLayout streckeSpieler2Layout = new javax.swing.GroupLayout(streckeSpieler2);
         streckeSpieler2.setLayout(streckeSpieler2Layout);
         streckeSpieler2Layout.setHorizontalGroup(
             streckeSpieler2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
+            .addGroup(streckeSpieler2Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(auto2, javax.swing.GroupLayout.DEFAULT_SIZE, 62, Short.MAX_VALUE)
+                .addGap(18, 18, 18))
         );
         streckeSpieler2Layout.setVerticalGroup(
             streckeSpieler2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, streckeSpieler2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(auto2, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
-        drehzahlMesser2.setOrientation(1);
+        bremsen2.setText("Bremsen");
+        bremsen2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bremsen2ActionPerformed(evt);
+            }
+        });
 
-        drehzahlMesser1.setOrientation(1);
-
-        schalten2.setText("Schalten");
-
-        schalten1.setText("Schalten");
+        bremsen1.setText("Bremsen");
 
         gasGeben2.setText("Gas geben");
         gasGeben2.addActionListener(new java.awt.event.ActionListener() {
@@ -104,6 +147,10 @@ public class MainMenu extends javax.swing.JFrame {
 
         countdown.setText("3, 2, 1");
 
+        weiter1.setText("weiter");
+
+        fertig.setText("fertig");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -115,84 +162,96 @@ public class MainMenu extends javax.swing.JFrame {
                         .addComponent(spielerName1, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(14, 14, 14)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(tankFuellung1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(tankFuellung1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(10, 10, 10)
+                                .addComponent(bremsen1, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(drehzahlMesser1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addComponent(schalten1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(gasGeben1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-                .addGap(8, 8, 8)
+                                .addComponent(gasGeben1))))
+                    .addComponent(weiter1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(22, 22, 22)
                 .addComponent(streckeSpieler1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(32, 32, 32)
+                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel1)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(24, 24, 24)
                         .addComponent(countdown)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
                 .addComponent(streckeSpieler2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(64, 64, 64)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(schalten2, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(drehzahlMesser2, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(tankFuellung2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(gasGeben2, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(spielerName2, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(35, 35, 35)))
-                .addGap(29, 29, 29))
+                        .addGap(64, 64, 64))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(fertig, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(bremsen2, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(gasGeben2, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(tankFuellung2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(20, 20, 20))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGap(45, 45, 45)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(spielerName1)
+                    .addComponent(spielerName2))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(45, 45, 45)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(spielerName1)
-                            .addComponent(spielerName2))
                         .addGap(43, 43, 43)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(drehzahlMesser1, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(tankFuellung2, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(drehzahlMesser2, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(tankFuellung1, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(schalten1)
-                            .addComponent(schalten2))
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(gasGeben2)
-                            .addComponent(gasGeben1))
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(streckeSpieler2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(streckeSpieler1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-                .addContainerGap())
+                            .addComponent(tankFuellung1, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(27, 27, 27)
+                                .addComponent(bremsen1)
+                                .addGap(82, 82, 82)
+                                .addComponent(gasGeben1))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(67, 67, 67)
+                        .addComponent(bremsen2)
+                        .addGap(77, 77, 77)
+                        .addComponent(gasGeben2)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(fertig, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(29, 29, 29))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(weiter1, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(24, 24, 24))))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 25, Short.MAX_VALUE)
+                .addGap(0, 32, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addGap(294, 294, 294)
                 .addComponent(countdown)
                 .addGap(73, 73, 73))
+            .addComponent(streckeSpieler2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(streckeSpieler1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void gasGeben2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_gasGeben2ActionPerformed
-        // TODO add your handling code here:
+        this.tankFuellung2.setValue((this.tankFuellung2.getValue() - 1));
     }//GEN-LAST:event_gasGeben2ActionPerformed
 
     private void gasGeben1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_gasGeben1ActionPerformed
-        // TODO add your handling code here:
+        this.tankFuellung1.setValue((this.tankFuellung1.getValue() - 1));
     }//GEN-LAST:event_gasGeben1ActionPerformed
+
+    private void bremsen2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bremsen2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_bremsen2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -230,19 +289,42 @@ public class MainMenu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel auto2;
+    private javax.swing.JButton bremsen1;
+    private javax.swing.JButton bremsen2;
     private javax.swing.JLabel countdown;
-    private javax.swing.JProgressBar drehzahlMesser1;
-    private javax.swing.JProgressBar drehzahlMesser2;
+    private javax.swing.JButton fertig;
     private javax.swing.JButton gasGeben1;
     private javax.swing.JButton gasGeben2;
+    private javax.swing.JFrame jFrame1;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JButton schalten1;
-    private javax.swing.JButton schalten2;
     private javax.swing.JLabel spielerName1;
     private javax.swing.JLabel spielerName2;
     private javax.swing.JPanel streckeSpieler1;
     private javax.swing.JPanel streckeSpieler2;
     private javax.swing.JProgressBar tankFuellung1;
     private javax.swing.JProgressBar tankFuellung2;
+    private javax.swing.JButton weiter1;
     // End of variables declaration//GEN-END:variables
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+                if (ke.getKeyCode() == KeyEvent.VK_S) {
+            System.out.println("Gedrückt");
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        if (ke.getKeyCode() == KeyEvent.VK_S) {
+            System.out.println("Gedrückt");
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+                if (ke.getKeyCode() == KeyEvent.VK_S) {
+            System.out.println("Gedrückt");
+        }
+    }
 }
